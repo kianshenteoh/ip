c1bb7c41d7ec8e2000875f9af91b6a31f19d89fc
kianshenteoh <kianshen12@gmail.com>
2025-08-21 14:04:00 +0800

    Renamed chatbot

---
+++ src/main/java/Mikey.java
@@ -0,0 +1,13 @@
+public class Mikey {
+    private static final String LINE = "___________________________________________________________";
+
+    private static void printLine() {
+        System.out.println(LINE);
+    }
+    public static void main(String[] args) {
+        printLine();
+        System.out.println("Hello, I'm Mikey! \nWhat can I do for you today?");
+        printLine();
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
