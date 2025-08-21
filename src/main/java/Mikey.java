b545e065b8d4e5e0bb5e5e776b03e4b7ead85350
kianshenteoh <kianshen12@gmail.com>
2025-08-21 15:24:39 +0800

    Mark as Done

--- src/main/java/Mikey.java
+++ src/main/java/Mikey.java
@@ -3,33 +3,53 @@ import java.util.ArrayList;
 
 public class Mikey {
     private static final String LINE = "  ___________________________________________________________";
-    private static final ArrayList<String> list = new ArrayList<>();
+    private static final TaskList taskList = new TaskList();
 
     private static void printLine() {
         System.out.println(LINE);
     }
+
+    private static void greet() {
+        printLine();
+        System.out.println("  Hello, I'm Mikey!");
+        System.out.println("  What can I do for you today?");
+        printLine();
+    }
+
+    private static void bye() {
+        printLine();
+        System.out.println("  Bye. Hope to see you again soon!");
+        printLine();
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        printLine();
-        System.out.println("  Hello, I'm Mikey! \n  What can I do for you today?");
-        printLine();
+        greet();
+
         String input = scanner.nextLine();
         while (!input.equals("bye")) {
             printLine();
             if (input.equals("list")) {
-                for (int i = 0; i < list.size(); i++) {
-                    int index = i + 1;
-                    System.out.println("  " + index + ": " + list.get(i));
-                }
+                taskList.printTasks();
                 printLine();
-            } else {
-                list.add(input);
-                System.out.println("  " + input);
+            } else if (input.startsWith("mark ")) {
+                String[] words = input.split(" ");
+                int index = Integer.parseInt(words[1]);
+                taskList.markTask(index - 1);
+                printLine();
+            } else if (input.startsWith("unmark ")) {
+                String[] words = input.split(" ");
+                int index = Integer.parseInt(words[1]);
+                taskList.unmarkTask(index - 1);
+                printLine();
+            }
+            else {
+                taskList.addTask(new Task(input));
                 printLine();
             }
             input = scanner.nextLine();
         }
-        System.out.println("Bye. Hope to see you again soon!");
+        bye();
         scanner.close();
     }
 }
---
+++ src/main/java/TaskList.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+public class TaskList {
+    private final ArrayList<Task> tasks = new ArrayList<>();
+
+    public void addTask(Task task) {
+        tasks.add(task);
+        System.out.println("Added " + task.getDescription());
+    }
+
+    public void markTask(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            Task currTask = tasks.get(index);
+            currTask.markDone();
+            System.out.println("  Nice! I've marked this task as done:");
+            System.out.println("    " + currTask);
+        } else {
+            System.out.println("Task does not exist!");
+        }
+    }
+
+    public void unmarkTask(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            Task currTask = tasks.get(index);
+            currTask.markUndone();
+            System.out.println("  Ok, I've marked this task as not done yet:");
+            System.out.println("    " + currTask);
+        } else {
+            System.out.println("Task does not exist!");
+        }
+    }
+
+    public void printTasks() {
+        if (tasks.isEmpty()) {
+            System.out.println("No tasks yet!");
+            return;
+        }
+        System.out.println("  Here are the tasks in your list:");
+        for (int i = 0; i < tasks.size(); i++) {
+            int index = i + 1;
+            Task currTask = tasks.get(i);
+            System.out.println("  " + index + ". " + currTask);
+        }
+    }
+
+    public int size() {
+        return tasks.size();
+    }
+}
